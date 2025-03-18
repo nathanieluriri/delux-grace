45234c4372528023267fdc4cd135a9e2a2697115
nathanieluriri <uririnathaniel@gmail.com>
2025-03-18 03:57:54 +0100

    removed unused carousel variable called setSlides

--- app/page.jsx
+++ app/page.jsx
@@ -17,7 +17,7 @@ import Autoplay from "embla-carousel-autoplay";
 export function EmblaCarousel() {
   const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay({ delay: 13000 })]); // Autoplay every 3s
   const [selectedIndex, setSelectedIndex] = useState(0);
-  const [slides, setSlides] = useState([0, 1,]); // Dummy slides
+  const [slides, ] = useState([0, 1,]); // Dummy slides
 
   // Handle Next and Prev
   const scrollPrev = useCallback(() => emblaApi && emblaApi.scrollPrev(), [emblaApi]);
