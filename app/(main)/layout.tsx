b7297c9f6118b3f22e937869df2a23aa47c7cb49
Joshua Ajorgbor <joshuaajorgbor@gmail.com>
2025-03-18 11:36:20 +0100

    created admin route group

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -5,12 +5,12 @@ import Header from "@/scaffold/Header";
 import Footer from "@/scaffold/Footer";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
